6e2fc80e931dd459fd433d9e690f01fcca2f5f2e
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-27 01:06:39 -0400

    Add Treasure Hunt README

--- treasure_hunt/spec/treasure_spec.rb
+++ treasure_hunt/spec/treasure_spec.rb
@@ -1,19 +1,3 @@
-# Do you like treasure hunts? 
-# In this problem you are to write a program to 
-# explore the above array for a treasure. 
-# The values in the array are clues. 
-# Each cell contains an integer between 11 and 55; 
-# for each value the ten's digit represents the row 
-# number and the unit's digit represents the column 
-# number of the cell containing the next clue. 
-# Starting in the upper left corner (at 1,1), 
-# use the clues to guide your search of the array. 
-# (The first three clues are 11, 34, 42). 
-# The treasure is a cell whose value is the same as its coordinates. 
-# Your program must first read in the treasure map data into a 5 by 5 array. 
-# Your program should output the cells it visits during its search, 
-# and a message indicating where you found the treasure.
-
 require_relative '../lib/treasure'
 
 describe "TreasureFinder" do
